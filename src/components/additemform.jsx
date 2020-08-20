baaef9aa74a137832d683a94c938b05f1f6b599f
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-08-20 12:05:08 +0100

    added CRUD add records

--- src/components/additemform.jsx
+++ src/components/additemform.jsx
@@ -3,14 +3,82 @@ import firebase from "firebase"
 
 const AddItemForm = () => {
 
-  return (
-    <form>
-      <input name="name" type="text" />
-      <input name="type" type="text" />
-      <input name="qty" type="number" />
-      <input name="description" type="text" />
-      <button>Submit</button>
-    </form>
-  )
-}
+    const [name, setName] = useState("")
+    const [image, setImage] = useState("")
+    const [description, setDescription] = useState("")
+    const [category, setCategory] = useState("")
+    const [level, setLevel] = useState("")
+    const [tags, setTags] = useState("")
+  
+    /* The onSubmit function we takes the 'e'
+      or event and submits it to Firebase
+      */
+    const onSubmit = e => {
+      /* 
+      preventDefault is important because it
+      prevents the whole page from reloading
+      */
+      e.preventDefault()
+      firebase
+        .firestore()
+        .collection("items")
+        .add({
+          name,
+          image,
+          description,
+          category,
+          level,
+          tags
+        })
+        //.then will reset the form to nothing
+        .then(() => setName(""), setImage(""), setDescription(''), setCategory(""), setLevel(""), setTags(""))
+    }
+  
+    return (
+      <form onSubmit={onSubmit}>
+        <input placeholder="Name"
+          value={name}
+          name="name"
+          /* onChange takes the event and set it to whatever
+          is currently in the input. 'e' is equal to the event
+          happening. currentTarget.value is what is inputted
+           */
+          onChange={e => setName(e.currentTarget.value)}
+          type="text"
+        />
+        <input placeholder="Image"
+          value={image}
+          name="image"
+          onChange={e => setImage(e.currentTarget.value)}
+          type="text"
+        />
+        <input placeholder="Description"
+          value={description}
+          name="Description"
+          onChange={e => setDescription(e.currentTarget.value)}
+          type="text"
+        />
+        <input placeholder="Category"
+          value={category}
+          name="category"
+          onChange={e => setCategory(e.currentTarget.value)}
+          type="text"
+        />
+        <input placeholder="Level"
+        value={level}
+        name="level"
+        onChange={e => setLevel(e.currentTarget.value)}
+        type="level"
+        />
+        <input placeholder="Tags"
+        value={tags}
+        name="tags"
+        onChange={e => setTags(e.currentTarget.value)}
+        type="tags"
+        />
+        <button>Submit</button>
+      </form>
+    )
+  }
+
 export default AddItemForm
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import firebase from "../components/firebase"
 import ItemList from "../components/itemList"
+import AddItemForm from "../components/addItemForm"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -17,6 +18,8 @@ const SecondPage = () => {
     <h2>List of Resources</h2>
     <h2>Add Resource</h2>
     <ItemList/>
+    <h2>Add Item</h2>
+    <AddItemForm/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
   )
