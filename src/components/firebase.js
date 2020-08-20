d566d004d48fd32ebc2b663e1512ce8cdef1e8d7
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-08-20 11:28:39 +0100

    added firebase database

---
+++ src/components/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app"
+import 'firebase/analytics';
+import 'firebase/auth'
+import 'firebase/firestore'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBzTSJa3narKK7jvmZ449cBgMz8NXJq1V4",
+    authDomain: "testproject-85401.firebaseapp.com",
+    databaseURL: "https://testproject-85401.firebaseio.com",
+    projectId: "testproject-85401",
+    storageBucket: "testproject-85401.appspot.com",
+    messagingSenderId: "497395027109",
+    appId: "1:497395027109:web:3a4f65f77ed66d92e3914c",
+    measurementId: "G-SNR4PBMFZ5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebase
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,37 @@
 import React from "react"
 import { Link } from "gatsby"
+import firebase from "../components/firebase"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage = () => {
+  firebase
+  .firestore()
+  .collection("items")
+  .add({
+    name: "Red Tails",
+    image: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-469442375-1588101206.jpg?crop=1.00xw:0.948xh;0,0.0517xh&resize=640:*",
+    resourceInfo: "This is an example learning resource",
+    category: "history",
+    level: "key stage one",
+    tags: "tags"
+  })
+  .then(ref => {
+    console.log("Added document with ID: ", ref.id)
+  })
+
+  return (
+
+  
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Resource Database</h1>
+    <h2>List of Resources</h2>
+    <h2>Add Resource</h2>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
-)
+  )
+}
 
 export default SecondPage
