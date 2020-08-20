06ff8404310b352e930963531bbf005c277b5711
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-08-20 11:46:12 +0100

    added basic crud table

---
+++ src/components/itemList.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react"
+import firebase from "./firebase"
+// import "../styles/global.css"
+
+const useItems = () => {
+    const [items, setItems] = useState([]); //useState() hook, sets initial state to an empty array
+    useEffect(() => {
+      firebase
+        .firestore() //access firestore
+        .collection("items") //access "items" collection
+        .onSnapshot(snapshot => {
+          //You can "listen" to a document with the onSnapshot() method.
+          const listItems = snapshot.docs.map(doc => ({
+            //map each document into snapshot
+            id: doc.id, //id and data pushed into items array
+            ...doc.data() //spread operator merges data to id.
+          }));
+          setItems(listItems); //items is equal to listItems
+        });
+    }, []);
+    return items;
+  };
+
+const ItemList = () => {
+    const listItem = useItems();
+  return (
+<table className="item-table">
+  <tbody>
+    <tr>
+      <th className="resource-name">Resource Name</th>
+      <th className="image">Image</th>
+      <th className="description">Description</th>
+      <th className="category">Category</th>
+      <th className="level">Level</th>
+      <th className="tags">Tags</th>
+    </tr>
+  </tbody>
+  {listItem.map(item => (
+        <tbody key={item.id}>
+          <tr>
+            <td className="resource-name">{item.name}</td>
+            <td className="image">{item.image}</td>
+            <td className="description">{item.description}</td>
+            <td className="category">{item.category}</td>
+            <td className="level">{item.level}</td>
+            <td className="tags">{item.tags}</td>
+          </tr>
+        </tbody>
+      ))}
+</table>
+)
+}
+export default ItemList
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,25 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 import firebase from "../components/firebase"
+import ItemList from "../components/itemList"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = () => {
-  firebase
-  .firestore()
-  .collection("items")
-  .add({
-    name: "Red Tails",
-    image: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-469442375-1588101206.jpg?crop=1.00xw:0.948xh;0,0.0517xh&resize=640:*",
-    resourceInfo: "This is an example learning resource",
-    category: "history",
-    level: "key stage one",
-    tags: "tags"
-  })
-  .then(ref => {
-    console.log("Added document with ID: ", ref.id)
-  })
 
   return (
 
@@ -29,6 +16,7 @@ const SecondPage = () => {
     <h1>Resource Database</h1>
     <h2>List of Resources</h2>
     <h2>Add Resource</h2>
+    <ItemList/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
   )
