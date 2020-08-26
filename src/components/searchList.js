522ec2c60044b6027450355e4a4fd45ac22f4d68
Dotsworthy <dotsworthy@gmail.com>
2020-08-26 14:49:09 +0100

    added category generation to resource catalogue

--- src/components/searchList.js
+++ src/components/searchList.js
@@ -1,34 +1,34 @@
 import React, { useState, useEffect } from "react"
 import firebase from "./firebase"
 
-// const useItems = () => {
-//     const [items, setItems] = useState([]); //useState() hook, sets initial state to an empty array
-//     useEffect(() => {
-//       firebase
-//         .firestore() //access firestore
-//         .collection("items") //access "items" collection
-//         .onSnapshot(snapshot => {
-//           //You can "listen" to a document with the onSnapshot() method.
-//           const listItems = snapshot.docs.map(doc => ({
-//             //map each document into snapshot
-//             id: doc.id, //id and data pushed into items array
-//             ...doc.data() //spread operator merges data to id.
-//           }));
-//           setItems(listItems); //items is equal to listItems
-//         });
-//         //called the unsubscribe--closing connection to Firestore.
-//         // return () => unsubscribe()
-//     }, []);
-//     return items;
-// }
+const useItems = () => {
+    const [categories, setItems] = useState([]); //useState() hook, sets initial state to an empty array
+    useEffect(() => {
+      firebase
+        .firestore() //access firestore
+        .collection("categories") //access "items" collection
+        .onSnapshot(snapshot => {
+          //You can "listen" to a document with the onSnapshot() method.
+          const listCategories = snapshot.docs.map(doc => ({
+            //map each document into snapshot
+            id: doc.id, //id and data pushed into items array
+            ...doc.data() //spread operator merges data to id.
+          }));
+          setItems(listCategories); //items is equal to listItems
+        });
+        //called the unsubscribe--closing connection to Firestore.
+        // return () => unsubscribe()
+    }, []);
+    return categories;
+}
 
 const SearchList = () => {
-    // const listitem = items;
+    const listCategory = useItems();
     return (
         <div className="resource-page-search">
-            <p>History</p>
-            <p>World War Two</p> 
-            <p>Politics</p>
+            {listCategory.map(category => (
+                <h3>{category.name}</h3>
+            ))}
         </div>
     )
 }
