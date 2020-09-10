d48542a51e7ea2b54427f0a881eadd0793a173ca
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-09-10 13:36:19 +0100

    removed firebase from resources page

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -3,9 +3,6 @@ import ItemCatalogue from "../components/itemCatalogue"
 import SearchList from "../components/searchList"
 import Layout from "../components/layout";
 import { Link } from "gatsby";
-import firebase from "../components/firebase"
-
-const database = firebase;
 
 const ResourcePage = () => {
     return (
@@ -13,7 +10,7 @@ const ResourcePage = () => {
             <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.."/>
             <div className="resource-page-container">
             <SearchList/>
-            <ItemCatalogue db={database} resource={"items"}/>
+            <ItemCatalogue/>
             </div>
             <Link to="/">Go back to the homepage</Link>
         </Layout>
