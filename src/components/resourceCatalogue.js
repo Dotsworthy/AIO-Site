d26ddbfc8eb24f754f09fa25360ceaa38f8d694f
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-09-24 00:56:34 +0100

    fixed some package lock issues and resolved slowdown. Issue is with setState on categories, will need new approach for rendering

--- src/components/resourceCatalogue.js
+++ src/components/resourceCatalogue.js
@@ -4,67 +4,103 @@ import 'firebase/storage'
 
 function ResourceCatalogue() {
   const [resources, setResources] = useState([]);
-  const [allCategories, setAllCategories] = useState([]);
-  const [allLevels, setAllLevels] = useState([]);
+  // const [allCategories, setAllCategories] = useState([]);
+  // const [allLevels, setAllLevels] = useState([]);
   const [category, setCategory] = useState("") ;
   const [level, setLevel] = useState("");
 
-  const CategoriesList = () => {
-    firebase
-    .firestore()
-    .collection("categories")
-    .onSnapshot(snapshot => {
-      const listCategories = snapshot.docs.map(doc => ({
+
+  const getFilters = async() => {
+    let list
+    const database = firebase.firestore();
+    await database.collection("categories")
+    .get()
+    .then(function(snapshot) {
+      list = snapshot.docs.map(doc => ({
         id: doc.id,
         ...doc.data()
-    }))
-    setAllCategories(listCategories)
-  })
-}
+      }));
+    })
+    .catch(function(error) {
+      console.log("Error getting documents: ", error)
+    })
+    return list
+  }
 
-const LevelsList = () => {
-  firebase
-  .firestore()
-  .collection("levels")
-  .onSnapshot(snapshot => {
-    const listLevels = snapshot.docs.map(doc => ({
-      id: doc.id,
-      ...doc.data()
-  }))
-  setAllLevels(listLevels)
-})
-}
-
-const getDownload = (download) => {
-  const storage = firebase.storage();
-  const storageRef = storage.ref()
-  const httpsReference = storageRef.child(download);
+  // const CategoriesList = () => {
+  //   let result
+  //   firebase
+  //   .firestore()
+  //   .collection("categories")
+  //   .onSnapshot(snapshot => {
+  //     result = snapshot.docs.map(doc => ({
+  //       id: doc.id,
+  //       ...doc.data()
+  //   }))
+  //   // console.log(result)
+  //   // return result
+  // })
+  // console.log(result)
+  // // return result
   
-  httpsReference.getDownloadURL().then(function(url) {
-    let xhr = new XMLHttpRequest();
-    xhr.responseType = 'blob';
-    xhr.onload = function(event) {
-    let a = document.createElement('a');
-        a.href = window.URL.createObjectURL(xhr.response);
-        a.download = `${download}`;
-        a.style.display = 'none';
-        document.body.appendChild(a);
-        a.click();   
-    };
-    xhr.open('GET', url);
-    xhr.send();
-  }).catch(function(error) {
-    console.log(error);
-  });
-}
+  // }
 
-useEffect(() => {
-  if (category && level) {
+  const categories = getFilters()
+  console.log(categories)
+
+// const LevelsList = () => {
+//   firebase
+//   .firestore()
+//   .collection("levels")
+//   .onSnapshot(snapshot => {
+//     const listLevels = snapshot.docs.map(doc => ({
+//       id: doc.id,
+//       ...doc.data()
+//   }))
+//   setAllLevels(listLevels)
+//   })
+//   }
+
+  const getDownload = (download) => {
+    const storage = firebase.storage();
+    const storageRef = storage.ref()
+    const httpsReference = storageRef.child(download);
+  
+    httpsReference.getDownloadURL().then(function(url) {
+      let xhr = new XMLHttpRequest();
+      xhr.responseType = 'blob';
+      xhr.onload = function(event) {
+      let a = document.createElement('a');
+          a.href = window.URL.createObjectURL(xhr.response);
+          a.download = `${download}`;
+          a.style.display = 'none';
+          document.body.appendChild(a);
+          a.click();   
+      };
+      xhr.open('GET', url);
+      xhr.send();
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  useEffect(() => {
+    if (category) {
+      firebase
+      .firestore()
+      .collection("items")
+      .where("category", "==", category)
+      .onSnapshot(snapshot => {
+        const listResources = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        setResources(listResources);
+      })
+  } else {
     firebase
     .firestore()
     .collection("items")
-    .where("category", "==", category)
-    .where("level", "==", level)
     .onSnapshot(snapshot => {
       const listResources = snapshot.docs.map(doc => ({
         id: doc.id,
@@ -72,93 +108,48 @@ useEffect(() => {
       }));
       setResources(listResources);
     })
-}
+    }
+  },[category]);
 
-if (category && !level) {
-  firebase
-  .firestore()
-  .collection("items")
-  .where("category", "==", category)
-  .onSnapshot(snapshot => {
-    const listResources = snapshot.docs.map(doc => ({
-      id: doc.id,
-      ...doc.data()
-    }));
-    setResources(listResources);
-  })
-}
-
-if (level && !category) {
-  firebase
-  .firestore()
-  .collection("items")
-  .where("level", "==", level)
-  .onSnapshot(snapshot => {
-    const listResources = snapshot.docs.map(doc => ({
-      id: doc.id,
-      ...doc.data()
-    }));
-    setResources(listResources);
-  })
-}
-
-if (!level && !category) {
-    firebase
-  .firestore()
-  .collection("items")
-  .onSnapshot(snapshot => {
-    const listResources = snapshot.docs.map(doc => ({
-      id: doc.id,
-      ...doc.data()
-    }));
-    setResources(listResources);
-  })
-}
-
-
-},[category, level]);
-
-return (
-  <div className="resource-page-container">
-    <div>
-      <div className="resource-page-filter">
-          <h3>Categories</h3>
-          <button className="button-active" onClick={() => setCategory("")}>Show All</button>  
-          {CategoriesList()}  
-          {allCategories.map(category => (
-              <button key={category.id} className="button" 
-              onClick={() => setCategory(category.name)}
-              >{category.name}</button>
-          ))}
+  return (
+    <div className="resource-page-container">
+      <div>
+        {/* <div className="resource-page-filter">
+            <h3>Categories</h3>
+            <button className="button-active" onClick={() => setCategory("")}>Show All</button>  
+            {categories.map(category => (
+                <button key={category.id} className="button" 
+                onClick={() => setCategory(category.name)}
+                >{category.name}</button>
+            ))}
+        </div> */}
+        {/* <div className="resource-page-filter">
+            <h3>Education Level</h3>
+            <button className="button-active" onClick={() => setLevel("")}>Show All</button>  
+            {LevelsList()}  
+            {allLevels.map(level => (
+                <button key={level.id} className="button" 
+                onClick={() => setLevel(level.name)}
+                >{level.name}</button>
+            ))}
+        </div> */}
       </div>
-      <div className="resource-page-filter">
-          <h3>Education Level</h3>
-          <button className="button-active" onClick={() => setLevel("")}>Show All</button>  
-          {LevelsList()}  
-          {allLevels.map(level => (
-              <button key={level.id} className="button" 
-              onClick={() => setLevel(level.name)}
-              >{level.name}</button>
-          ))}
+    <div className ="resource-page-items">
+        {resources.map(resource => (
+          <div key={resource.id} className="catalogue-item">
+              <h3>{resource.name}</h3>
+              <div className="catalogue-image-container">
+              <img className="catalogue-image" src={resource.image} alt={resource.name}></img>
+              </div>
+              <div className="catalogue-button">
+              <button>More Info</button>
+              <button onClick={() => getDownload(resource.download)}>Download</button>
+              </div>
+          </div>      
+        ))}
       </div>
     </div>
-  <div className ="resource-page-items">
-      {resources.map(resource => (
-        <div key={resource.id} className="catalogue-item">
-            <h3>{resource.name}</h3>
-            <div className="catalogue-image-container">
-            <img className="catalogue-image" src={resource.image} alt={resource.name}></img>
-            </div>
-            <div className="catalogue-button">
-            <button>More Info</button>
-            <button onClick={() => getDownload(resource.download)}>Download</button>
-            </div>
-        </div>      
-      ))}
-    </div>
-  </div>
-)
-
+  )
 }
 
 export default ResourceCatalogue
