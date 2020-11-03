2e9e20c9c112182c0152b6bbfa32b8b8d5b7e8b0
Dotsworthy <dotsworthy@gmail.com>
2020-11-03 17:15:15 +0000

    safety commit. user can update all categories, laid groundwork for function that will allow for seeing how many resources are attached to the category

--- src/components/listDatabaseItems.js
+++ src/components/listDatabaseItems.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react"
 import firebase from "./firebase"
 
+const db = firebase.firestore()
+
 const ListDatabaseItems = ( { collection, resourceEntry, editItem } ) => {
     const [items, setItems] = useState([])
+    // const [resources, setResources] = useState([])
     // const [deleting, setDeleting] = useState(false)
     // const [editing, setEditing] = useState(false)
 
@@ -17,9 +20,39 @@ const ListDatabaseItems = ( { collection, resourceEntry, editItem } ) => {
         return unsubscribe
       }, [collection])
     
+    useEffect(() => {
+        
+    })  
     // const deleteItem = (item) => {
     //   setDeleting(true)  
     // }
+
+    // items.map(function(item) {
+    //     const length = useItems(item)
+    //     const name = "resourcesAttached"
+    //     const value = length
+
+    //     setItems({...items[item.id], [name]: value})
+    // })
+
+
+    // const getResourcesAttached = async (item) => {
+    //     const resources = [];
+
+    //     const resourcesRef = db.collection("items")
+    //     const snapshot = await resourcesRef.where(`${resourceEntry}`, "==", `${item}`).get();
+    //     if (snapshot.empty) {
+    //         return
+    //     }
+
+    //     snapshot.forEach(doc => {
+    //         resources.push(doc.data())
+    //     })
+        
+    //     console.log(resources);
+    //     return resources
+    // }
+
     
     return (
     <div>
@@ -27,12 +60,15 @@ const ListDatabaseItems = ( { collection, resourceEntry, editItem } ) => {
       <tbody>
         <tr className="header-row">
           <th className="name">Name</th>
+          <th className="resources-attached">Resources Attached</th>
         </tr>
       </tbody>
       {items.map(item => (
             <tbody key={item.id}>
               <tr className="data-row">
-                <td className="resource-name">{item.name}</td>
+                <td className="item-name">{item.name}</td>
+                
+                <td className="item-resources-attached">placeholder</td>
                 <td className="buttons">
                     <button onClick={() => editItem(item, collection, resourceEntry)}>Edit</button>
                     {/* <button onClick={() => deleteItem(item)}>Delete</button> */}
