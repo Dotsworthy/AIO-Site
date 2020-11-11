09a0b23da78c53a73c29778fb280bfd332b05fef
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-11-11 13:35:38 +0000

    delete component now waits for function to delete all items first

--- src/components/deleteResource.js
+++ src/components/deleteResource.js
@@ -4,64 +4,53 @@ import 'firebase/storage'
 
 const DeleteResource = ({ currentResource, setDeleting }) => {
 
-    const resource = currentResource
+  const resource = currentResource
 
-    // const deleteItem = (id, imageRef, downloadRef, imageLocation, downloadLocation) => {
-    //     deleteFile(imageRef, id, imageLocation)
-    //     deleteAllFiles(downloadRef, id, downloadLocation)
-    
-    //     firebase
-    //       .firestore()
-    //       .collection("items")
-    //       .doc(id)
-    //       .delete()
-    //   } 
+  const onSubmit = async e => {
+    e.preventDefault()
+    await deleteFile(resource.image, resource.id, "images")
+    await deleteAllFiles(resource.download, resource.id, "downloads")
 
-    const onSubmit = e => {
-      e.preventDefault()
-      deleteFile(resource.image, resource.id, "images")
-      deleteAllFiles(resource.download, resource.id, "downloads")
+    firebase
+    .firestore()
+    .collection("items")
+    .doc(resource.id)
+    .delete()
 
-      firebase
-      .firestore()
-      .collection("items")
-      .doc(resource.id)
-      .delete()
+    setDeleting(false)
+  }  
+  
+  // deletes a single file within a folder in firebase storage
+  const deleteFile = async (file, id, location) => {
+    const storageRef = firebase.storage().ref()
+    const fileRef = storageRef.child(`${location}/${id}/${file}`)
+    fileRef.delete().then(function() {
+      console.log("resource deleted successfully")
+    }).catch(function(error) {
+      console.log(error)
+    })
+  }
+  
+  // deletes all files within a folder in firebase storage
+  const deleteAllFiles = async (file, id, location) => {
+    const storageRef = firebase.storage().ref()
+    file.forEach(download => {
+      const fileRef = storageRef.child(`${location}/${id}/${download}`)
+      fileRef.delete().then(function() {
+        console.log("resource deleted successfully")
+      }).catch(function(error) {
+        console.log(error)
+      })
+    })
+  }
 
-      setDeleting(false)
-    }  
-    
-      const deleteFile = (file, id, location) => {
-        const storageRef = firebase.storage().ref()
-        const fileRef = storageRef.child(`${location}/${id}/${file}`)
-        fileRef.delete().then(function() {
-          console.log("resource deleted successfully")
-        }).catch(function(error) {
-          console.log(error)
-        })
-      }
-    
-      const deleteAllFiles = (file, id, location) => {
-        const storageRef = firebase.storage().ref()
-        file.forEach(download => {
-          const fileRef = storageRef.child(`${location}/${id}/${download}`)
-          fileRef.delete().then(function() {
-            console.log("resource deleted successfully")
-          }).catch(function(error) {
-            console.log(error)
-          })
-        })
-    
-        setDeleting(false);
-      }
-
-    return (
-        <form onSubmit={onSubmit}>
-        <p>Are you sure you want to delete this resource? This cannot be reversed!</p>
-        <button type="button" onClick={() => setDeleting(false)}>Cancel</button>
-        <button type="submit">Confirm</button>
-        </form>
-    )
+  return (
+      <form onSubmit={onSubmit}>
+      <p>Are you sure you want to delete this resource? This cannot be reversed!</p>
+      <button type="button" onClick={() => setDeleting(false)}>Cancel</button>
+      <button type="submit">Confirm</button>
+      </form>
+  )
 }
 
 export default DeleteResource
