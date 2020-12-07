77e923147059d65c262166e521f9eeb4f2572517
Dotsworthy <dotsworthy@gmail.com>
2020-12-07 15:17:40 +0000

    added navbar code@

--- src/components/header.js
+++ src/components/header.js
@@ -8,18 +8,14 @@ const Header = ({ siteTitle }) => (
 
   <header>
     <nav className="nav-bar">
-      <span className="navbar-toggle" id="js-navbar-toggle">
-      <FontAwesomeIcon icon={faBars} class="fas fa-bars" size="1x"/>
-      </span>
 
-      <Link className="logo" to="/">{siteTitle}</Link>
-      <ul class="nav-main" id="js-menu">
-        <li><Link className="nav-links" to="/about-us">About Us</Link></li>
-        <li><Link className="nav-links" to="/take-action">Take Action</Link></li>
-        <li><Link className="nav-links" to="/resources/">Resources</Link> </li>
-        <li><Link className="nav-links" to="/contact-us">Contact Us</Link> </li>
-        <li><Link className="nav-links" to="/admin/">Admin</Link></li>
-      </ul>
+      {/* <Link className="logo" to="/">{siteTitle}</Link>
+        <Link className="nav-links" to="/about-us">About Us</Link>
+        <Link className="nav-links" to="/take-action">Take Action</Link>
+        <Link className="nav-links" to="/resources/">Resources</Link>
+        <Link className="nav-links" to="/contact-us">Contact Us</Link>
+        <Link className="nav-links" to="/admin/">Admin</Link> */}
+      
     </nav>
   </header>
 )
