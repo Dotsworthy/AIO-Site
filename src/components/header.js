2e0030b6228fb5bba7dadb031a72e6c33a62d611
Dotsworthy <dotsworthy@gmail.com>
2020-12-07 15:14:07 +0000

    added navigation menu directory

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,26 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div className="header-container">
-        <div className="header-title">
-        <Link className="link-title" to="/">{siteTitle}</Link>
-        </div>
 
-        <div className="header-links">
-        <Link className="header-link" to="/about-us">About Us</Link>
-        <Link className="header-link" to="/take-action">Take Action</Link>
-        <Link className="header-link" to="/resources/">Resources</Link>
-        <Link className="header-link" to="/contact-us">Contact Us</Link>
-        <Link className="header-link" to="/admin/">Admin</Link>
-        </div>
-    </div>
+  <header>
+    <nav className="nav-bar">
+      <span className="navbar-toggle" id="js-navbar-toggle">
+      <FontAwesomeIcon icon={faBars} class="fas fa-bars" size="1x"/>
+      </span>
+
+      <Link className="logo" to="/">{siteTitle}</Link>
+      <ul class="nav-main" id="js-menu">
+        <li><Link className="nav-links" to="/about-us">About Us</Link></li>
+        <li><Link className="nav-links" to="/take-action">Take Action</Link></li>
+        <li><Link className="nav-links" to="/resources/">Resources</Link> </li>
+        <li><Link className="nav-links" to="/contact-us">Contact Us</Link> </li>
+        <li><Link className="nav-links" to="/admin/">Admin</Link></li>
+      </ul>
+    </nav>
   </header>
 )
 
