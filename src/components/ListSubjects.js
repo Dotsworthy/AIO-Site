aafc31a2f2a0746bed2d53945a14cbe5edeed97d
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-12-15 14:23:50 +0000

    added spaces to tags on listsubjects

--- src/components/ListSubjects.js
+++ src/components/ListSubjects.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react"
 import DeleteSubject from "./deleteSubject"
 import UpdateSubject from "./updateSubject"
-import { Link } from "@reach/router"
 import { navigate } from "gatsby"
 import firebase from "./firebase"
 // import 'firebase/storage'
@@ -13,18 +12,17 @@ const ListSubjects = () => {
   // used for rendering and filtering resources
   const [resources, setResources] = useState([]);
   const [searchTerm, setSearchTerm] = useState(null)
-  const [searchLocation, setSearchLocation] = useState()
   const [orderBy, setOrderBy] = useState("name")
 
   // used for deleting resources
   const [deleting, setDeleting] = useState(false)
   const [editing, setEditing] = useState(false)
   const [currentItem, setCurrentItem] = useState([])
-
+ 
   // renders the list of resources
   useEffect(() => {
     if (searchTerm) {
-        const unsubscribe = firebase.firestore().collection("subjects").onSnapshot(snapshot => {
+        const unsubscribe = firebase.firestore().collection("subjects").orderBy(orderBy).onSnapshot(snapshot => {
           const listResources = snapshot.docs.map(doc => ({
             id: doc.id,
             ...doc.data()
@@ -52,7 +50,7 @@ const ListSubjects = () => {
       });
       return unsubscribe
     }
-  }, [editing, searchTerm])
+  }, [editing, searchTerm, orderBy])
 
   // sets Resource to be passed to deleteResource component
   const deleteSubject = (item) => {
@@ -89,16 +87,12 @@ const ListSubjects = () => {
     e.preventDefault()
     const element = document.getElementById("search").value
     setSearchTerm(element);
-    const category = document.getElementById("location").value
-    // setSearchLocation(category)
   }
 
   const addSubject = e => {
     e.preventDefault()
     navigate("/admin/subjectList/addSubject")
   }
-
-
 
   return (
     
@@ -109,12 +103,6 @@ const ListSubjects = () => {
         <button onClick={((e) => addSubject(e))}>Add Subject</button>
         <form className="database-navigation-content" onSubmit={onSubmit}>
           <input type="text" id="search" name="search" placeholder="Search"/>
-          <select id="location" name="location">
-            <option value="name">Resource Name</option>
-            <option value="category">Category</option>
-            <option value="level">Level</option>
-            <option value="tags">Tags</option>
-          </select>
         
         <div className="form-footer">
         <button type="submit">Search</button>
@@ -150,7 +138,7 @@ const ListSubjects = () => {
               <td className="resource-name">{resource.name}</td>
               <td className="category">{resource.category}</td>
               <td className="level">{resource.level}</td>
-              <td className="tags">{resource.tags.toString()}</td>
+              <td className="tags">{resource.tags.join(', ')}</td>
               <td className="table-buttons">
                 <button onClick={() => editSubject(resource)}>Edit</button>
                 <button onClick={() => deleteSubject(resource)}>Delete</button>
