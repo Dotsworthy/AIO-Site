50545c72d2d8d3808c930e8031ae90f45fbbb571
Dotsworthy <dotsworthy@gmail.com>
2020-12-17 09:59:48 +0000

    safety commit. downloading as zip requires promise to fire

--- src/components/downloadHandler.js
+++ src/components/downloadHandler.js
@@ -34,69 +34,73 @@ const DownloadHandler = ({ currentItem, setDownloading }) => {
         });
     }
 
+    const fileDownloader = async (fileList) => {
+        let zip = new JSZip();
+        let downloadList = zip.folder(`${item.name}`)
+        const storageRef = firebase.storage().ref()
+        await Promise.all(fileList.map(file => {
+            console.log("code reached")
+            const result = storageRef.child(`images/${item.id}/${file}`).getDownloadURL().then(function(url) {
+                console.log("code reached")
+                return new Promise(function(resolve, reject) {
+                    let xhr = new XMLHttpRequest();
+                    xhr.responseType = 'blob';
+                    xhr.onload = function(event) {
+                        let blob = xhr.response;
+                        downloadList.file(`${file}`, blob)
+                        resolve(xhr.response);
+                    }
+                })
+            })
+            
+        })
+    ).then(function() {
+        console.log("code reached")
+        console.log(downloadList);
+        console.log(zip);
+        return zip
+    })}
+
+
+
     const getAllDownloads = async (location) => {
-        const storage = firebase.storage();
-        const storageRef = storage.ref()
+        const storageRef = firebase.storage().ref();
         const download = document.getElementById("zip")
         download.value = "downloading..."
-        let zip = new JSZip();
-        let materials = zip.folder(`${item.name}`)
-        
-        let count = 0;
+        // let zip = new JSZip();
+        // let materials = zip.folder(`${item.name}`)
+        const fileList = Array.from(item.download)
 
-
-            // return new Promise((resolve, reject) => {
-            //     const downloadTask = httpsReference.getDownloadURL().then(function(url) {
-            //         let xhr = new XMLHttpRequest();
-            //         xhr.responseType = 'blob';
-            //         console.log(xhr);
-            //         xhr.onload = function(event) {
-            //             let blob = xhr.response;
-            //             console.log(blob);
-            //             materials.file(`${resource}, blob`)
-                        
-            //         };
-            //         xhr.open('GET', url)
-            //         xhr.send()
-            //     }).then(function complete() {
-            //         resolve(downloadTask);
-            //         console.log("fired")
-            //     })
-            //     .catch(function(error) {
-            //         console.log(error)
-            //     })
-            //     })
+        const zip = await fileDownloader(fileList);
         
-        // console.log(materials);
-        
-        item.download.forEach(resource => {
-            const httpsReference = storageRef.child(`${location}/${item.id}/${resource}`)
-            
-            httpsReference.getDownloadURL().then(function(url) {
-                let xhr = new XMLHttpRequest();
-                xhr.responseType = 'blob';
-                console.log(xhr)
-                xhr.onload = function(event) {
-                    let blob = xhr.response;
-                    materials.file(`${resource}`, blob)
-                };
-                xhr.open('GET', url)
-                xhr.send();  
-                count++
-                console.log(count)
-                }).catch(function(error) {
-                    console.log(error)
-                })   
-        })
-        // console.log(zip)
+        // await item.download.forEach(resource => {
+        //     const httpsReference = storageRef.child(`${location}/${item.id}/${resource}`)
+        //     const result = httpsReference.getDownloadURL().then(function(url) {
+        //         return new Promise(function(resolve, reject) {
+        //             let xhr = new XMLHttpRequest();
+        //             xhr.responseType = 'blob';
+        //             xhr.onload = function(event) {
+        //                 let blob = xhr.response;
+        //                 resolve(xhr.response);
+        //                 materials.file(`${resource}`, blob)
+        //                 console.log(materials)
+        //             };
+        //             xhr.open('GET', url)
+        //             xhr.send();  
+        //         })
+                
+        //         }).catch(function(error) {
+        //             console.log(error)
+        //         })   
+        // })
 
-        if (count == item.download.length) {
-            zip.generateAsync({type:"blob"})
-        .then(function (blob) {
-            saveAs(blob, `${item.name}`)
-            download.value = "Download";
-        })
-        }
+        console.log(zip);
+
+        // zip.generateAsync({type:"blob"})
+        // .then(function (blob) {
+        //     saveAs(blob, `${item.name}`)
+        //     download.value = "Download";
+        // })
 
     }  
 
