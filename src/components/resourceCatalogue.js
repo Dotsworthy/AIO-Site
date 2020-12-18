62d5e3a5e5d2e7cca47e367c214f132b0e076e58
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2020-12-18 13:58:04 +0000

    basic desktop theme for resource catalogue

--- src/components/resourceCatalogue.js
+++ src/components/resourceCatalogue.js
@@ -299,10 +299,13 @@ const ResourceCatalogue = ( ) => {
             <div className="catalogue-item-header">
               <h3>Categories</h3>
             </div>
-              <div className="form-inside-content">
-              <div className="content-search">
+            <div className="content-search">
                 <form><input type="text" id="search-filter categories1" onChange={() => filterButtons("categories1")} placeholder="search categories"/></form>
               </div>
+              <div 
+              className="form-inside-content"
+              >
+              
               {/* <button className="filter-button" onClick={() => setCategorySelected("")}>Show All</button>   */}
               
               
@@ -319,10 +322,13 @@ const ResourceCatalogue = ( ) => {
             <div className="catalogue-item-header">
               <h3>Education Level</h3>
               </div>
-              <div className="form-inside-content">
               <div className="content-search">
                 <form><input type="text" id="search-filter levels1" onChange={() => filterButtons("levels1")} placeholder="search levels"/></form>
               </div>
+              <div 
+              className="form-inside-content"
+              >
+              
               {/* <button className="filter-button" onClick={() => setLevelSelected("")}>Show All</button>     */}
               {allLevels.map(level => (
                   <button style={ level.name === levelSelected ? {color: "red"} : {color: "black"}} key={level.id} className="filter-button levels1" 
@@ -335,10 +341,13 @@ const ResourceCatalogue = ( ) => {
             <div className = "catalogue-item-header">
               <h3>Tags</h3>
               </div>
-              <div className="form-inside-content">
               <div className="content-search">
                 <form><input type="text" id="search-filter tags1" onChange={() => filterButtons("tags1")} placeholder="search tags"/></form>
               </div>
+              <div
+               className="form-inside-content"
+              >
+              
               {/* <button className="filter-button" onClick={() => setTagSelected("")}>Show All</button>     */}
               {allTags.map(tag => (
                   <button style={ tag.name === tagSelected ? {color: "red"} : {color: "black"}} key={tag.id} className="filter-button tags1" 
@@ -380,7 +389,7 @@ const ResourceCatalogue = ( ) => {
     {moreInfo && <MoreInfo currentItem={currentItem} setMoreInfo={setMoreInfo}/>}
     {downloading && 
     <div className="catalogue-download" id="white">
-      <div className="catalogue-item-header">
+      <div className="more-info-item-header">
                     <h2>Downloads</h2>
                     <button className="mobile-button" onClick={() => setDownloading(false)}>X</button>
                 </div>   
--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import ResourceCatalogue from "../components/resourceCatalogue"
 import Layout from "../components/layout";
-import DownloadHandler from "../components/downloadHandler";
+
 
 // Move Download handler to resource catalogue.
 
@@ -11,32 +11,22 @@ const ResourcePage = () => {
         { id: null, name: "", download: "" },
         ]
 
-    const [downloading, setDownloading] = useState(false);
+    
     const [currentItem, setCurrentItem] = useState(initialItemState)
 
-    const downloadResource = item => {
-        setDownloading(true);
-        setCurrentItem({
-            id: item.id,
-            name: item.name,
-            download: item.download
-        })
-    }
+    
 
     return (
         <Layout siteType={"client"}>
-            <div className="banner-container-desktop-only" id="yellow">
+            <div className="banner-container-desktop-only" id="dark-grey">
                     <div className="paragraph-box" id="white"><h1>RESOURCE CATALOGUE</h1></div>
             </div>
 
             <div>
             {/* <input type="text" id="myInput" onKeyUp="myFunction()" placeholder="Search for names.."/> */}
             <div>
-            <ResourceCatalogue downloadResource={downloadResource} />
-            {downloading && <DownloadHandler
-            currentItem={currentItem}
-            setDownloading={setDownloading}
-            />}
+            <ResourceCatalogue/>
+            
             </div>
             </div>
         </Layout>
