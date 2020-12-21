5d028352f3a83b7ac684accb346f99b48c0e196f
Dotsworthy <dotsworthy@gmail.com>
2020-12-21 20:46:26 +0000

    fixed node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,27 +33,27 @@ exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
     }
   }
 
-  exports.createPages = async function ({ actions, graphql }) {
-    const { data } = await graphql(`
-    query {
-      allMarkdownRemark {
-        nodes {
-          fields {
-            slug
-          }
-        }
-      }
-    }
-    `)
-    data.allMarkdownRemark.forEach(node => {
-      const slug = node.fields.slug
-      actions.createPage({
-        path: slug,
-        component: require.resolve(`./src/templates/blog-post.js`),
-        context: { slug: slug},
-      })
-    })
-  }
+  // exports.createPages = async function ({ actions, graphql }) {
+  //   const { data } = await graphql(`
+  //   query {
+  //     allMarkdownRemark {
+  //       nodes {
+  //         fields {
+  //           slug
+  //         }
+  //       }
+  //     }
+  //   }
+  //   `)
+  //   data.allMarkdownRemark.forEach(node => {
+  //     const slug = node.fields.slug
+  //     actions.createPage({
+  //       path: slug,
+  //       component: require.resolve(`./src/templates/blog-post.js`),
+  //       context: { slug: slug},
+  //     })
+  //   })
+  // }
 
   exports.onCreatePage = async ({page, actions}) => {
     const { createPage } = actions
