91f1cc22715814ad9732a1cfed96feff33ca6709
Dotsworthy <dotsworthy@gmail.com>
2020-12-22 19:13:40 +0000

    added mount check for resource catalogue and min-height so it renders correctly

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,19 +1,23 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import ResourceCatalogue from "../components/resourceCatalogue"
 import Layout from "../components/layout";
-
 
 // Move Download handler to resource catalogue.
 
 const ResourcePage = () => {
 
+    const [hasMounted, setHasMounted] = useState(false);
+
+    useEffect(() => {
+        setHasMounted(true);
+    }, [])
+
+
+
     return (
         <Layout siteType={"client"}>
-            <div className="banner-container-desktop-only" id="dark-grey">
-                    <div className="paragraph-box" id="white"><h1>RESOURCE CATALOGUE</h1></div>
-            </div>
 
-            <ResourceCatalogue/>
+        {hasMounted && <ResourceCatalogue/>} 
 
         </Layout>
     )
