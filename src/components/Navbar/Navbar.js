dd9537a4bd884642f7f15a01de85b31d5d5f8ceb
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2021-01-04 16:57:49 +0000

    deleted logo file

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -26,31 +26,30 @@ const Navbar = ( siteType ) => {
   return (
     <nav className="navigation" id={ site === "client" ? "blue" : "red"}>
       <Link className="home-link" to="/">All In One</Link>
-      <button className="navbox-toggle" id="toggle"
-        onClick={() => triggerNavbar()}
-      >
+      <button className="navbox-toggle" id="toggle" onClick={() => triggerNavbar()}>
         {navbarOpen ? 
         <div className="burger-menu-container"id="burger-open">
           <div className="slice1" id="slice1"/>
-            <div className="slice2" id="slice2"/>
-            <div className="slice3" id="slice3"/>
+          <div className="slice2" id="slice2"/>
+          <div className="slice3" id="slice3"/>
         </div> 
-        : <div className="burger-menu-container" id="burger-closed">
-            <div className="slice1"/>
-            <div className="slice2"/>
-            <div className="slice3"/>
-          </div>
-          }
+        : 
+        <div className="burger-menu-container" id="burger-closed">
+          <div className="slice1"/>
+          <div className="slice2"/>
+          <div className="slice3"/>
+        </div>
+        }
       </button>
-      {navbarOpen ? (
+      {navbarOpen ? 
         <div className="navbox" id="navbox-open">
           <NavbarLinks siteType={ site } setNavbarOpen={setNavbarOpen}/>
         </div>
-      ) : (
+       : 
         <div className="navbox" id="navbox-closed">
           <NavbarLinks siteType = { site } setNavbarOpen={setNavbarOpen}/>
         </div>
-      )}
+      }
     </nav>
   )
 }
--- src/components/Navbar/NavbarLinks.js
+++ src/components/Navbar/NavbarLinks.js
@@ -18,39 +18,39 @@ const NavbarLinks = ( siteType ) => {
 
 
   return (
-      <>
+    <>
       { siteType.siteType === "client" && 
         <>
-        <Link className="navbar-link" to="/about-us">About Us</Link>
-        <Link className="navbar-link" to="/take-action">Take Action</Link>
-        <Link className="navbar-link" to="/resources/">Resources</Link>
-        <Link className="navbar-link" to="/contact-us">Contact Us</Link>
-        <Link className="navbar-link" id="admin-login" to="/admin/">Admin Login</Link>
+          <Link className="navbar-link" to="/about-us">About Us</Link>
+          <Link className="navbar-link" to="/take-action">Take Action</Link>
+          <Link className="navbar-link" to="/resources/">Resources</Link>
+          <Link className="navbar-link" to="/contact-us">Contact Us</Link>
+          <Link className="navbar-link" id="admin-login" to="/admin/">Admin Login</Link>
         </>
       }
       { siteType.siteType === "admin" && 
       <>
-      <Link className="navbar-link" to="/admin/subjectList">Subject List</Link>
-      <Link className="navbar-link" to="/admin/categoryList">Categories</Link>
-      <Link className="navbar-link" to="/admin/levelList">Levels</Link>
-      <Link className="navbar-link" to="/admin/tagList">Tags</Link>
+        <Link className="navbar-link" to="/admin/subjectList">Subject List</Link>
+        <Link className="navbar-link" to="/admin/categoryList">Categories</Link>
+        <Link className="navbar-link" to="/admin/levelList">Levels</Link>
+        <Link className="navbar-link" to="/admin/tagList">Tags</Link>
       </>
       }
       <div className="icons-navlinks">
-            <a className="icon-link" href="https://www.facebook.com/allinoneeducationuk">
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
+        <a className="icon-link" href="https://www.facebook.com/allinoneeducationuk">
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
             
-            <a className="icon-link" href="https://twitter.com/aioeducation?lang=en">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
+        <a className="icon-link" href="https://twitter.com/aioeducation?lang=en">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
 
-            <a className="icon-link"  href="https://www.instagram.com/allinoneeducationuk/">
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
+        <a className="icon-link"  href="https://www.instagram.com/allinoneeducationuk/">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
       </div>
-      </>
-    )
-  }
+    </>
+  )
+}
   
   export default NavbarLinks
