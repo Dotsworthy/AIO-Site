134be2bef91f89eff986d5e884842d0755ed7015
Andrew Watson <58689469+Dotsworthy@users.noreply.github.com>
2021-01-18 12:58:31 +0000

    tweaked catalogue description for desktop

--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -12,7 +12,7 @@ const MoreInfo = ({ currentItem, setDownloading, setMoreInfo }) => {
         const storageRef = firebase.storage().ref(image);
         storageRef.getDownloadURL().then(function(url) {
             
-          const img = document.getElementById("output")
+          const img = document.getElementById("more-info-image")
           img.src = url;
         }).catch(function(error) {
             console.log(error);
@@ -22,25 +22,31 @@ const MoreInfo = ({ currentItem, setDownloading, setMoreInfo }) => {
     return (
         <div className="catalogue-moreInfo" id="white">
             
-            <div className="more-info-item-header">
-                <h2>{item.name}</h2>
-                <button className="mobile-button" onClick={() => setMoreInfo(false)}>X</button>
-            </div>
-
-                <div className="catalogue-item-info">
-                <div className="catalogue-image-container">
-                <img className="preview-image" src={getImageURL(item.id, "images", item)} id={"output"} alt={item.name}></img>
+            {/* <div className="more-info-item-header"> */}
+                
+                
+            {/* </div> */}
+                <div className="more-info-content">
+                <div className="more-info-image-container">
+                <img className="preview-image" src={getImageURL(item.id, "images", item)} id={"more-info-image"} alt={item.name}></img>
                 </div>
                 <div className="catalogue-description">
-                <p>{item.description}</p>
+                <div className="catalogue-header">
+                <h2>{item.name}</h2>
+                  <button className="mobile-button" onClick={() => setMoreInfo(false)}>X</button>
+                  
                 </div>
-                
+                <p>{item.description}</p>
+                <DownloadHandler currentItem={currentItem} setDownloading={setDownloading} setMoreInfo={setMoreInfo}/>
+                </div>  
                 </div>
 
-                <div className="catalogue-item-header">
-                    <h2>Downloads</h2>
-                </div>    
-                    <DownloadHandler currentItem={currentItem} setDownloading={setDownloading} setMoreInfo={setMoreInfo}/>
+                
+                
+               
+
+                 
+                    
                 {/* </div> */}
                 </div>
             
